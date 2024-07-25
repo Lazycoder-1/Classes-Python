0f0b84ebe982c9c42dfd32a50251331e6d82c6f6
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-07-25 17:27:28 +0000

    Add files via upload

---
+++ 90-change-variables-object-class.py
@@ -0,0 +1,26 @@
+
+# using variables that changes constantly like age,prices
+class Person_age:
+    def __init__(self,age):
+        self.age = age
+    def age_increase(self):
+        self.age += 1
+    def display_age(self):
+        print('Your age is {}'.format(self.age))
+ohene = Person_age(23)
+print(ohene.age_increase())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
