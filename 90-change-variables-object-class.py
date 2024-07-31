445c649b336e0fa55a4b2bf42d0cb78380ec13e3
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-07-31 05:27:27 +0000

    Add files via upload
    Added the change attributes to classes in here

--- 90-change-variables-object-class.py
+++ 90-change-variables-object-class.py
@@ -1,14 +1,23 @@
 
 # using variables that changes constantly like age,prices
-class Person_age:
-    def __init__(self,age):
+# class Person_age:
+#     def __init__(self,age):
+#         self.age = age
+#     def age_increase(self):
+#         self.age += 1
+#     def display_age(self):
+#         print('Your age is {}'.format(self.age))
+# ohene = Person_age(23)
+# print(ohene.age_increase())
+
+class Dogs:
+    def __init__(self,name,age):
         self.age = age
-    def age_increase(self):
-        self.age += 1
-    def display_age(self):
-        print('Your age is {}'.format(self.age))
-ohene = Person_age(23)
-print(ohene.age_increase())
+        self.name = name
+
+
+knotty = Dogs('knotty',1)
+print(knotty.name)
 
 
 
